bffd816bae1a723fdddc66c0756f4018ddcd1736
yusbastia <107796472+yusbastia@users.noreply.github.com>
2022-11-13 12:08:44 +0800

    Add files via upload

---
+++ crypt.tcl
@@ -0,0 +1,29 @@
+#encrypter/decrypter tcl by Krasi
+#email: kcdenkov@gmail.com
+#webpage: http://freeunibg.eu/
+#modification by Krasi
+
+
+bind dcc n encrypt dcc:encrypt
+bind dcc n decrypt dcc:decrypt
+proc dcc:encrypt {handle idx arg} {
+  if {$arg == ""} {
+    putidx $idx "* Usage: .encrypt <key> <string>"
+    return 0
+  }
+  set dakey [lindex $arg 0]
+  set datext [lindex $arg 1]
+  putidx $idx "* Encrypted output: [encrypt $dakey $datext]"
+  return 0
+}
+proc dcc:decrypt {handle idx arg} {
+  if {$arg == ""} {
+    putidx $idx "* Usage: .decrypt <key> <string>"
+    return 0
+  }
+  set dakey [lindex $arg 0]
+  set datext [lindex $arg 1]
+  putidx $idx "Decrypted output: [decrypt $dakey $datext]"
+  return 0
+}
+putlog "* encryter/decrypter by |i-XeS-i|"
