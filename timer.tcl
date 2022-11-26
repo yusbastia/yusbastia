bae41e3bc09f0e8bb7eaf3804df2726b3b7e9ead
yusbastia <107796472+yusbastia@users.noreply.github.com>
2022-11-26 15:28:27 +0800

    Update timer.tcl

--- timer.tcl
+++ timer.tcl
@@ -1,22 +1,26 @@
-set timemessage_setting(message) {
-	"This is a message."
-	"You can add as many lines as you wish."
+set clock_chans {
+    "#basechanel"
 }
-set timemessage_setting(channel) "#basechanel #chan2"
 
-set timemessage_setting(time) "5"
+# Posochete prez kakav interval ot vreme da go kazva (v minuti)
+set interval 30
 
-if {![string match 1.6.* $version]} { putlog "\002TIMEMESSAGE:\002 \002WARNING:\002 This script is intended to run on eggdrop 1.6.x or later." }
-if {[info tclversion] < 8.2} { putlog "\002TIMEMESSAGE:\002 \002WARNING:\002 This script is intended to run on Tcl Version 8.2 or later." }
+############################################################################
 
-if {[string compare [string index $timemessage_setting(time) 0] "!"] == 0} { set timemessage_timer [string range $timemessage_setting(time) 1 end] } { set timemessage_timer [expr $timemessage_setting(time) * 60] }
-if {[lsearch -glob [utimers] "* timemessage_go *"] == -1} { utimer $timemessage_timer timemessage_go }
+bind pub - !time say_time
+timer $interval vreme
 
-proc timemessage_go {} {
-	foreach chan $::timemessage_setting(channel) {
-		if {![validchan $chan] || ![botonchan $chan]} { continue }
-		foreach line $::timemessage_setting(message) { putserv "PRIVMSG $chan :$line" }
-	}
-	if {[lsearch -glob [utimers] "* timemessage_go *"] == -1} { utimer $::timemessage_timer timemessage_go }
+proc vreme {} {
+    global interval clock_chans
+    set Kalimera [time]
+    foreach zaek $clock_chans {
+	putserv "PRIVMSG $zaek :Chasut e $Kalimera :>"
+    }
+    timer $interval vreme
 }
-putlog "\002TIMEMESSAGE:\002 TimeMessage.tcl Version 1.0 by Wcc is loaded."
+
+proc say_time {nick uhost hand kanal kom} {
+    set chas [time]
+    puthelp "PRIVMSG $kanal :Chasut e $chas ;>"
+    return 0
+}
