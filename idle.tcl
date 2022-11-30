1899f49bb5f0a313e5744abd6bf905cf99d56946
yusbastia <107796472+yusbastia@users.noreply.github.com>
2022-11-30 12:44:51 +0800

    Update idle.tcl

--- idle.tcl
+++ idle.tcl
@@ -1,58 +1,44 @@
+# Editor : Lemon
+# Server : irc.chating.id
+##################################################
+
+bind pub - ".i" do:whois
 
 
-# Nov 29, 2012
+proc do:whois {nick uhost handle chan text} {
+global workingchan
 
-# reference:  http://forum.egghelp.org/viewtopic.php?t=19178
+   if {"$text" == ""} {
+      putserv "privmsg $chan :Syntax: .i <nick> "
+      return
+      }
 
+   bind raw - 317 watch:317
+   bind raw - 402 watch:402
 
-# Hero
-#   
-# New postPosted: Today at 1:13 pm    Post subject: Need Idle Check TCL    Reply with quote
-# Hello I Wanna Request Idle Check Tcl
-# Like:
-# <+Hero> !Idle Hero
-# <&Bot> Hero Idle Time Is: 5hrs 20mins 36secs
-# Thanks In Advance
-
-
-
-
-###################### Method - have bot do /whois on user, and get the idle time from return.  Should give resolution in seconds, as requested ########
-
-
-bind pub - ".idle" get_idle_time_whois
-
-proc get_idle_time_whois {nick uhost handle chan text} {
-global working_chan
-
-   if {[lindex [split $text] 0] == ""} {
-      putserv "privmsg $chan :Syntax: !idle <nick>"
-      return 0
-         }
-
-   set user [lindex [split $text] 0]
-
-   if {![onchan $user $chan]} {
-      putserv "privmsg $chan :Sorry, $user is not on $chan"
-      return 0
-         }
-   
-   set working_chan $chan
-
-   bind raw - 317 got_whois
-
-   putserv "whois $user $user"
-
-
+   putserv "whois [split $text 0] [split $text 0]"
+   set workingchan $chan
 }
 
 
-proc got_whois {from keyword text} {
-global working_chan
-   
-   putserv "privmsg $working_chan :[lindex [split $text] 1] idle time is: [duration [lindex [split $text] 2]]"   
+proc watch:317 {from keyword text} {
+global workingchan
 
+   putserv "privmsg $workingchan :[lindex [split $text] 1] has been idle [duration [lindex [split $text] 2]] \[signon [ctime [lindex [split $text] 3]]\]"
 
-   utimer 5 [list unbind raw - 317 got_whois]
- 
+   unbind raw - 317 watch:317
+   unbind raw - 402 watch:402
 }
+
+
+proc watch:402 {from keyword text} {
+global workingchan
+
+   putserv "privmsg $workingchan :\"[string trimleft [lindex $text 2] :] [lrange [split $text] 3 end]\""
+
+   unbind raw - 317 watch:317
+   unbind raw - 402 watch:402
+}
+
+
+putlog "Loaded Idle"
