fb22fb35885c72ed7c1b691274936e63d01bbf3e
yusbastia <107796472+yusbastia@users.noreply.github.com>
2022-12-01 13:18:18 +0800

    Update detik.tcl

--- detik.tcl
+++ detik.tcl
@@ -1,19 +1,11 @@
-########################################
-# News Gateways for www.detik.com
-# Dibuat oleh: AresU[at]Bosen.NET
-# Create Date: 01/06/2002 21:52
-# Update Date: 04/11/2004 02:14
-# Anda diperbolehkan melakukan perubahan pada program tetapi tidak pada Copyright Header ini.
-# Program ini boleh di distribusikan sebebas-bebasnya tanpa ada batasnya sampek beeebbbaaaasss sekali :-)
-# Ucapan terima kasih untuk temen2xku:
-#   Bosen, TioEuy, SakitJiwa, Brain, syzwz, HeltZ, Petrix, w4n, Alphacentury, Zen, all family on #romance@centrin.net.id
-# Our base at: http://www.bosen.net/releases
-########################################
+###################################
+# News Gateways for www.detik.com #
+###################################
 
 bind pub - !news pub_news
+bind msg m !newsuser msg:newsadduser
 
-set detikchan "#yobayat"
-set detiktimer 20
+set fnewsuser scripts/newsuser.txt
 set countday 0
 set ketemu 0
 set nbody 0
@@ -25,15 +17,8 @@ set newsurl ""
 set goturl 0
 set hariini ""
 set newsnick ""
-set newschan ""
 
-proc timernews_start {} {
-  global detiktimer
-  if {[string match *timernews_read* [timers]]} {return 0}
-  timer $detiktimer timernews_read
-}
-
-timernews_start
+timer 4 timernews
 
 set mhari {
  Senin
@@ -55,15 +40,30 @@ set mday {
  Sun
 }
 
-proc timernews_read {} {
-  global detiktimer
-  pub_news "" "" "" "" ""
-  timer $detiktimer timernews_read
+proc timernews {} {
+   pub_news "" "" "" "" ""
+   timer 4 timernews
 }
 
-proc pub_news {nick uhost hand chan arg} {
-global botnick countday ketemu nbody tmparg newsdone newsurl goturl mhari mday hariini newsnick limetitle yellotitle newschan
+proc pub_news {nick uhost hand channel arg} {
+global botnick countday ketemu nbody tmparg newsdone newsurl goturl mhari mday hariini fnewsuser newsnick limetitle yellotitle
 
+   if {![file exists $fnewsuser]} {
+        set fd [open $fnewsuser w]
+        close $fd
+   }
+   set fd [open $fnewsuser r]
+   set j 0
+   while {![eof $fd]} {
+     lappend newsusers [gets $fd]
+     set j [incr j] 
+   }
+   close $fd
+   
+   if {[lsearch $newsusers $nick] == -1 && $nick != ""} {
+     putserv "NOTICE $nick :\0030,12 NEWS \00311,1 Sekali² Beli Koran Dong :p "      
+     return 0
+   }
    set countday 0
    set ketemu 0
    set nbody 0
@@ -75,21 +75,25 @@ global botnick countday ketemu nbody tmparg newsdone newsurl goturl mhari mday h
    set newsurl ""
    set hariini [lindex $mhari [lsearch $mday [string range [ctime [unixtime]] 0 2]]]
    set newsnick $nick
-   set newschan [string tolower $chan]
-  
-   putlog "Connection to http://jkt1.detik.com"
+   
+   if {$newsnick != ""} {
+      putlog "\[NEWS\] <$newsnick>!news"
+      putserv "NOTICE $newsnick :\0030,12 NEWS \0038,1 News Gateways"
+      putserv "NOTICE $newsnick :\0030,12 NEWS \0038,1 Please wait when reading news from detik.com ..."
+   }
+   
+   putlog "Connection to http://www.detik.com"
    catch {connect jkt1.detik.com 80} tidx
    control $tidx newsbuffer
-   putdcc $tidx "GET /index.htm HTTP/1.0"
-   putdcc $tidx "HOST: jkt1.detik.com"
+   putdcc $tidx "GET /index.php HTTP/1.1"
+   putdcc $tidx "HOST: www.detik.com"
    putdcc $tidx ""
    putlog "..."
    return
 }
 
 proc newsbuffer {idx arg} {
-global countday ketemu nbody tmparg limetitle yellotitle newsdone newsurl goturl hariini newsnick detikchan newschan
-global detikchan detiktimer
+global countday ketemu nbody tmparg limetitle yellotitle newsdone newsurl goturl hariini newsnick
 
    if {[string first $hariini $arg] != -1} {
          incr countday
@@ -99,69 +103,63 @@ global detikchan detiktimer
    }
    set nurl [string first "class=\"hl\"" $arg]
    if {$ketemu == 1 && $goturl == 0 && $nurl != -1} {
-     set nurl [expr $nurl -180]
-     set tmpurl [string range $arg $nurl 200]
+     set nurl [expr $nurl -130]
+     set tmpurl [string range $arg $nurl 150]
      set nurl [expr [string first "href" $tmpurl] +6]
      set nakhir [expr [string first "\" class" $tmpurl] -1]
      set newsurl [string range $tmpurl $nurl $nakhir]
-     if {[string length $newsurl] > 120} {
-       set nurl [expr [string first "url" $newsurl] + 4]
-       set nakhir [string length $newsurl]
-       set newsurl [string range $newsurl $nurl $nakhir]
-     }
      set goturl 1
    }
    set nlime [string first "subjudul" $arg]
    if {$ketemu == 1 && $nlime != -1} {
-     putlog "\[NEWS\] Get Lime Title"
-     set nlime [expr $nlime +10]
-     set nakhir [expr [string first "\<" $arg] -1]
-     set limetitle [string range $arg $nlime $nakhir]
+	putlog "\[NEWS\] Get Lime Title"
+   	set nlime [expr $nlime +10]
+   	set nakhir [expr [string first "\<" $arg] -1]
+   	set limetitle [string range $arg $nlime $nakhir]
    }
    set nyello [string first "strJudul" $arg]
    if {$ketemu == 1 && $nyello != -1} {
-     putlog "\[NEWS\] Get Yello Title"
-     set nyello [expr $nyello +10]
-     set tmpyello [string range $arg $nyello [expr $nyello + 100]]
-     set nakhir [expr [string first "\<" $tmpyello] -1]
-     set yellotitle [string range $tmpyello 0 $nakhir]
-     set nbody 1
-     set tmparg $arg
+	putlog "\[NEWS\] Get Yello Title"
+   	set nyello [expr $nyello +10]
+	set tmpyello [string range $arg $nyello [expr $nyello + 100]]
+   	set nakhir [expr [string first "\<" $tmpyello] -1]
+   	set yellotitle [string range $tmpyello 0 $nakhir]
+	set nbody 1
+	set tmparg $arg
    }
    if {$nbody == 1 && $tmparg != $arg} {
-   set nawal [expr [string first "summary" $arg] +9]
-   set nakhir [expr [string first "</span>" $arg] -1]
-   set newsbody $arg
-   if {$nakhir > 1} {
-   set newsbody [string range $arg $nawal $nakhir]
-   }
-   set newsdone 1
-   killdcc $idx
+   	set nawal [expr [string first "summary" $arg] +9]
+   	set nakhir [expr [string first "</span>" $arg] -1]
+   	set newsbody $arg
+   	if {$nakhir > 1} {
+   	set newsbody [string range $arg $nawal $nakhir]
+   	}
+   	set newsdone 1
+   	killdcc $idx
    }
    if {$newsdone == 1} {
-     putlog "\[NEWS\] Download News Done"
-     if {$newsnick == ""} {
-       if {$limetitle == ""} {
-         foreach zz $detikchan {
-           putserv "PRIVMSG $zz :\0034,1\[DETIKnews\]\0038,1 $yellotitle\00315,1 $newsbody\00311,1 $newsurl"
-         }
-       } else {
-         foreach zz $detikchan {
-           putserv "PRIVMSG $zz :\0034,1\[DETIKnews\]\0037,1 $limetitle\0038,1 $yellotitle\00315,1 $newsbody\00311,1 $newsurl"
-         }
-       }
-     }
-     if {$newsnick != ""} {
-       foreach zz $detikchan {
-         if {$newschan == $zz} {
-           if {$limetitle == ""} {
-             putserv "PRIVMSG $newsnick :\0034,1\[DETIKnews\]\0038,1 $yellotitle\00315,1 $newsbody\00311,1 $newsurl"
-           } else {
-             putserv "PRIVMSG $newsnick :\0034,1\[DETIKnews\]\0037,1 $limetitle\0038,1 $yellotitle\00315,1 $newsbody\00311,1 $newsurl"
+      putlog "\[NEWS\] Download News Done"
+      if {$newsnick == ""} {
+           foreach ruang [channels] {
+              putserv "PRIVMSG $ruang :\0034,9\[DETIKnews\] \0039,2$limetitle \0038,2$yellotitle \0030,2$newsbody \00311,2$newsurl"
            }
-         }
-       }
-     }
+      } else {
+              putserv "NOTICE $newsnick :\0034,9\[DETIKnews\] \0039,2$limetitle \0038,2$yellotitle \0030,2$newsbody \00311,2$newsurl"
+      }
    }
    return 0
 }
+
+proc msg:newsadduser {nick uhost hand arg} {
+     global fnewsuser
+
+     putlog "\[NEWS\] <$nick> !newsadduser"
+     set fd [open $fnewsuser a]
+     puts $fd $arg
+     close $fd
+     putserv "NOTICE $nick :\0030,12 NEWS \00311,1 New User ---> \0038$arg "
+     putserv "NOTICE $arg :\0030,12 NEWS \0038,1 Welcome to Our News Gateways"
+     putserv "NOTICE $arg :\0030,12 NEWS \0038,1 Type !news to get news from detik.com, remember this is realtime news"
+     return
+}
+
