1cde641d579f8c1249017b5a3de9c096093fbd10
yulemon <107796472+yusbastia@users.noreply.github.com>
2022-12-15 14:44:14 +0800

    Update away.tcl

--- away.tcl
+++ away.tcl
@@ -1,178 +1,30 @@
-# away!
-set awaymsgs {
-  "(nonton dvd) (MiRC 4Lemon Log-4ON)"
-  "(godain tetangga) (MiRC 4Lemon Log-4ON)"
-  "(lagi kuliah) (MiRC 4Lemon Log-4ON)"
-  "(lagi nyari ulet bulu) (MiRC 4Lemon Log-4ON)"
+#         Script : Sex Story v1.0 by vian
+#                  Copyright 2009
+# Many thanks to you who use this TCL if you dont edit my copyright!
+
+bind pub * !story join:story 
+proc join:story {nick uhost hand chan rest} { 
+  global story_timers 
+  set story "sex.txt" 
+  if {![info exists story_timers($story)]} { 
+    slowmsg $story 
+    } else { 
+    puthelp "privmsg $nick :already reading..." 
+  } 
+} 
+proc slowmsg {file {pos 0}} { 
+  global story_timers 
+  set f [open $file] 
+  seek $f $pos 
+  if {[gets $f line]>-1} { 
+    putserv "privmsg #tomohon :$line" 
+    set story_timers($file) [utimer 11 [list slowmsg $file [tell $f]]] 
+    } else { 
+    utimer 15 [list putserv "Re-reading the story:"] 
+    set story_timers($file) [utimer 16 [list slowmsg $file]] 
+  } 
+  close $f 
 }
 
-set backmsgs {
-"12[4Tips12]: Lebih baik menjadi PELACUR berjiwa PELAJAR, daripada menjadi PELAJAR berjiwa PELACUR"
-"12[4Tips12]: Kebersihan itu adalah sebagian daripada iman..."
-"12[4Tips12]: Sekejam-kejamnya seorang laki-laki, pasti ada wajah ibu dimatanya ..."
-"12[4Tips12]: Rejeki itu bukannya dikejar ... tapi dijemput..."
-"12[4Tips12]: Uang itu kakinya empat .. kalo dikejar, dia akan lari ..."
-"12[4Tips12]: Cinta itu seperti kurap, makin di garuk makin sedap ... :)"
-"12[4Tips12]: Cobalah untuk lebih bijaksana dalam menyikapi setiap masalah, kadang diam itu emas ...:)"
-"12[4Tips12]: Jangan terlalu cepat menyalahkan seseorang dalam kesalahannya, ingat : tiada yang sempurna ..."
-"12[4Tips12]: MEROKOK DAPAT MENYEBABKAN KANKER, SERANGAN JANTUNG. IMPOTENSI DAN GANGGUAN KEHAMILAN DAN JANIN ..."
-"12[4Tips12]: Jangan suka membaca dalam gelap..."
-"12[4Tips12]: Kalo buang air besar, harap cebok nya yang bersih ya ... hehe"
-"12[4Tips12]: TELITI SEBELUM MEMBELI ..."
-"12[4Tips12]: Kalo penish tumbuhnya di kening, pasti kening pake celana ... ya gak ...hehe"
-"12[4Tips12]: Jadilah dirimu sendiri, jangan meniru2 orang lain ..."
-"12[4Tips12]: Kalo chatting jangan lupa waktu yah ... makan jangan telat ... :)"
-"12[4Tips12]: Menikahi orang yang kita cintai itu mudah, tapi yang sulit adalah mencintai orang yang kita nikahi ... pikirkan !!"
-"12[4Tips12]: Hanya cinta yang bisa merubah dunia ..."
-"12[4Tips12]: Hidup tanpa cinta bagaikan malam tanpa bintang ..."
-"12[4Tips12]: SEKALI MERDEKA TETAP MERDEKA ...!!!"
-"12[4Tips12]: Tuhan tidak akan merubah nasib seseorang apabila orang tsb tidak mau merubahnya sendiri ..."
-"12[4Tips12]: Pada akhirnya, semua orang akan duduk diporsinya masing-masing ... percayalah...Konidin ... :))"
-"12[4Tips12]: Sakit kepala ??? ... Benturin aja ke dinding ...pasti sembuh ... percaya deh ... :)"
-"12[4Tips12]: Buat cowo2 : Utamakan ibumu ... ibumu ... dan ibumu ... percayalah, pintumu akan terbuka ..."
-"12[4Tips12]: Wanita itu hanya perlu di mengerti ... dan laki-laki hanya perlu kesabaran ... itu kunci dari sebuah hubungan yang langgeng ..."
-"12[4Tips12]: Kita telah lahir ...  dan pasti akan mati .... yahhh ... itulah hidup ... sebuah kenyataan yang harus kita terima dengan ... keikhlasan ..."
-"12[4Tips12]: Jangan di ajari tentara baris ... :)"
-"12[4Tips12]: Kalo anda terserang Flu, berarti daya tahan tubuh anda sedang turun, perbanyak istirahat dan minum vitamin C ..."
-"12[4Tips12]: Sex itu dapat menghilangkan stress, karena sex dapat mengeluarkan hormon endorphen di otak kita, yaitu suatu hormon yang berfungsi memberi rasa nyaman di tubuh kita ..."
-"12[4Tips12]: Jangan lupa pake pengaman yahhh ... ;)"
-"12[4Tips12]: Sebuah survei menyebutkan bahwa sembilan dari sepuluh orang menunjukkan tanda kerusakan pada alat pendengaran, setelah menghabiskan sepanjang malam mendengarkan musik keras di klub atau pub"
-"12[4Tips12]: Rokok bisa menurunkan kualitas sperma bahkan merusak sperma ..."
-"12[4Tips12]: Cantik belum tentu Sexy ... tapi Sexy sudah pasti Cantik ... mau pilih yang mana, hayyooo  ;)"
-"12[4Tips12]: Bukan cantik yang bikin cinta, tapi cinta lah yang bikin cantik ..."
-"12[4Tips12]: Sebenarnya.. seks dan orgasme merupakan sebuah cara bagi tubuh kita untuk melepaskan stress dan kecemasan yang menumpuk sepanjang hari sejalan dengan aktivitas yang padat ..."
-"12[4Tips12]: Anak-anak yang menderita asma memiliki resiko yang lebih tinggi untuk mengalami penyakit psikologi ..."
-"12[4Tips12]: Bina lah hubungan baik anda, sebab konflik sangat mudah terjadi karena kurangnya komunikasi dan sikap memahami antara anda dan mereka ..."
-"12[4Tips12]: Waktu terus berjalan, gak ada rewind nya, jadi ... jangan tanggung2 !!! ... basah, basah sekalian !!!...  :)"
-"12[4Tips12]: Jadi kacang jangan lupa kulitnya ... Sudah kaya jangan lupa miskinnya ... key friend ... :)"
-"12[4Tips12]: Semua butuh proses waktu ... mengalirlah seperti air ... biarkan waktu yang menjawabnya ...."
-"12[4Tips12]: Pertama : Niat ... Kedua : Berusahalah semaksimal mungkin ... Ketiga : Serahkan semuanya pada Yang Kuasa ..."
-"12[4Tips12]: Jangan hitung rejeki orang .. hitung rejeki sendiri ... syukuri, dan nikmati ..."
-"12[4Tips12]: Jangan pernah takut untuk salah, karena dari salah kita tahu benar ..."
-"12[4Tips12]: Waktu adalah uang ..."
-"12[4Tips12]: Tuhan meletakkan mata kita di depan, bukan disamping, jadi ... Tataplah kedepan !! ... jangan ke belakang ..."
-"12[4Tips12]: Pikirkanlah semua resiko baik-buruk nya apa yang mau kita lakukan, jangan main hajar aja ..."
-"12[4Tips12]: Perduli setan orang mau ngomong apa ... Jadilah dirimu sendiri ..."
-"12[4Tips12]: Bila kita lari dari suatu masalah, berarti kita akan menambah masalah baru ..."
-"12[4Tips12]: Yang penting bukan masalahnya, tapi jalan keluarnya ..."
-"12[4Tips12]: Biarkan cinta berlalu, jangan biarkan membara, walaupun fatamorgana, tapi ... cinta itu ... nyata ..."
-"12[4Tips12]: Berniat menikah saja sudah berpahala, apalagi menikah, sebab menikah adalah ibadah, jadi, jangan takut untuk menikah ... maka ... menikahlah !!!"
-"12[4Tips12]: Mantan istri atau suami ada, tapi kalo mantan anak itu tidak pernah ada ..."
-"12[4Tips12]: Kalo cinta sudah melekat, taik kambing pun rasa coklat ..."
-"12[4Tips12]: Lebih baik berjalan di belakang pantat singa, daripada berjalan di belakang pantat wanita ... ;)"
-"12[4Tips12]: Anda tidak bisa tidur ???? .... Masturbasi lah !!!"
-"12[4Tips12]: Jangan pernah mengatakan cinta sebelum anda bisa membuktikannya ..."
-"12[4Tips12]: Begitu mudah untuk mengatakan cinta, semudah mengingkarinya ..."
-"12[4Tips12]: Cinta pasti datang, dan cinta juga pasti akan pergi dalam GARIS kehidupanmu ..."
-"12[4Tips12]: Hidup ini bukan untuk mencari penderitaan, tapi kebahagiaan ... walau kadang harus melewati sebuah penderitaan ..."
-"12[4Tips12]: Tegaslah !! ... Bila kita ingin menjadi diri kita sendiri ... Toh cepat atau lambat akhirnya dunia akan mengakuinya ..."
-"12[4Tips12]: Jika kita sungguh-sungguh menginginkan cinta, maka cintalah pada akhirnya yang justru menunggu kita."
-"12[4Tips12]: Orang yang tidak bisa memaafkan orang lain sama saja dengan orang yang memutuskan jembatan yang harus dilaluinya, karena semua orang perlu di maafkan"
-"12[4Tips12]: Ada dua hal yang harus Anda lupakan: Kebaikan yang Anda lakukan kepada orang lain dan kesalahan orang lain kepada Anda."
-"12[4Tips12]: Suatu kehidupan yang penuh kesalahan tak hanya lebih berharga namun juga lebih berguna dibandingkan hidup tanpa melakukan apapun"
-"12[4Tips12]: Pengetahuan ada dua macam : yang telah kita ketahui dengan sendirinya atau yang hanya kita ketahui dimana ia bisa didapatkan."
-"12[4Tips12]: Kita tidak dapat memperoleh pikiran yang damai kecuali kita dapat berhubungan dengan Sumber kedamaian yang ada di dalam diri kita. Damai yang kamu miliki dalam dirimu, dan jika kamu mencarinya di luar, kamu tidak akan pernah menemukannya..."
-"12[4Tips12]: Jika kita melayani dengan penyerahan penuh (bhakti), kita selamanya terpuaskan, dan merasa terpenuhi. Kita tidak akan berpikir untuk meminta apapun, karena kita merasa bahwa kita memiliki segala sesuatu dalam kelimpahan..."
-"12[4Tips12]: kita belajar bersemangat pantang mundur dan berlatih keberanian diri..."
-"12[4Tips12]: Carilah uang secukupnya saja untuk membiayai kehidupan, agar dapat menyisihkan waktu dan tenaga untuk berlatih rohani..."
-"12[4Tips12]: Kita bekerja tidak semata-mata untuk mencari uang, melainkan untuk menunaikan kewajiban kita..."
-"12[4Tips12]: Membantu orang lain adalah membantu kita sendiri. Kita senantiasa harus membersihkan pikiran kita, mengamalkan cinta kasih dan kasih sayang kita dengan tanpa syarat, tidak mengharapkan sesuatu imbalan, itulah jalan terbaik..."
-"12[4Tips12]: Membantu orang untuk mencapai Kebebasan Abadi dan mengenal Ajaran Sejati adalah bantuan yang terbesar, jasa yang tertinggi..."
-"12[4Tips12]: Jika masih memikirkan untuk membela nama baik kita, wibawa kita, agar orang tahu bahwa kita sangat baik, maka kita masih mempunyai ke-aku-an, masih belum terbebas dari konsepsi orang awam..."
-"12[4Tips12]: Teguran dari orang lain merupakan suatu pujian!..'
-"12[4Tips12]: Manusia terlalu sombong, kesombongan adalah karma kita yang paling merepotkan, rintangan yang terbesar..."
-"12[4Tips12]: Berterus terang kepada diri sendiri adalah berterus terang kepada siapapun..."
-"12[4Tips12]: Kita harus menemukan kekuatan kasih dalam diri kita terlebih dahulu, barulah kita dapat benar-benar mengasihi orang lain..."
-"12[4Tips12]: Jika kita tulus dan berdisiplin terhadap diri sendiri, maka orang lain akan jujur terhadap kita..."
-"12[4Tips12]: Carilah dulu Kerajaan Surga di dalam diri kita, kemudian kita akan memiliki segalanya..."
-"12[4Tips12]: Memperbaiki diri kita adalah memperbaiki dunia..."
-"12[4Tips12]: Jika kita menganggap diri kita lemah, maka kita akan menjadi lemah; jika kita menganggap diri kita agung, maka kita akan menjadi agung..."
-"12[4Tips12]: Kita harus dapat menghargai diri sendiri, baru kita akan dihormati oleh orang lain..."
-"12[4Tips12]: Perasaan bertobat dan rendah hati sangatlah penting, namun mencintai diri sendiri jauh lebih penting, jika Anda tidak dapat mencintai Anda sendiri, dapatkah Anda mencintai orang lain?..'
-"12[4Tips12]: Lebih baik merugikan diri daripada melukai orang lain, ini adalah kebenaran yang selalu tidak boleh kita lupakan, meskipun peraturan disiplin yang lain kurang kita perhatikan, itu bukan masalah besar..."
-"12[4Tips12]: Jika kita tidak mengubah diri dengan latihan rohani, kita tidak dapat menghindari karma kita sendiri..."
-"12[4Tips12]: Apakah karma perintang itu? Sesuatu yang tidak patut kita buat, yang akan merugikan kita sendiri, orang lain dan semua ciptaan. Setelah kita berbuat, kita akan menerima ganjaran dari Hukum Sebab Akibat..."
-"12[4Tips12]: Vegetarian bukan saja dari cara makan, tetapi yang terpenting adalah vegetarian secara pikiran, ucapan dan perbuatan..."
-}
 
-# set this variable to increase or decrease the max ammount of 
-# time (in mins) allowed till the next away is called for. default
-# is set to 2 hours. "set rtime 180" for 3 hours and so on.
-
-set rtime 10
-
-# Probibly no need for you to edit anything below here
-
-
-proc hide_away {args} {
-  global botnick awaymsgs backmsgs cycle rtime
-  putlog "MiRC Log-ON: Away System!"
-  set cycle [rand 10]
-  set dtime [rand $rtime ]
-  if {$dtime < 5 } {
-    set dtime 10
-  }
-  if {$cycle < 8} {
-    putserv "away :[lindex $awaymsgs [rand [llength $awaymsgs]]]" 
-    timer $dtime "hide_away"
-    return 0
-  }
-  if {$cycle >= 8 } {
-    set chans [channels]
-    putserv "away"
-    set backmsg "[lindex $backmsgs [rand [llength $backmsgs]]]" 
-    foreach chan $chans {
-      putserv "privmsg $chan :$backmsg"
-    }
-    timer $dtime "hide_away"
-    return 0
-  }
-}
-
-##### GENERAL SETTINGS ####
-# on the old scripts, it's on particular channel. but now, i made it for all channels.
-set channel "*"
-
-# Edit the time cycle which is in minutes format depending on the time intervals you want the bot to flow out the advertisment
-set time 20
-
-# EDIT the text or REMOVE or ADD lines including inverted commas at the starting and ending at each line 
-set text {
-  "0,2 15[0JASA15] 0Webhosting & Domain Registration. 11Kepingin Punya Web Sendiri? 0- Hosting, domain dan web desain. Mulai dari 135rb per tahun! 8klik www.dirmanto.com 0atau www.dirmanto.web.id"
-  "0,2 15[0INFORMASI15] 0Cari atau Baca Artikel 11kuliner, tutorial, lowongan kerja DLL 8klik www.dirmanto.web.id 0jangan lupa isi komentarnya yah :)"
-  "0,9<3>9,3<1>9,1<11,1 psyBNC, Eggdrop, Shell dan bantuan dasar IRC cari aja di http://dirmanto.web.id/ 9>1,3<9>3,9<0>"
-  "0,13<6>13,6<1>6,1<15,1 Kamu² kepingin punya blog pribadi dengan alamat nama-kamu.com ?8 klik http://www.dirmanto.web.id 6,1>1,6<13>6,13<0>"
-}
-
-##### DO NOT EDIT ANYTHING BELOW THIS LINE UNLESS YOU KNOW WHAT YOU ARE DOING #####
-
-if {[string compare [string index $time 0] "!"] == 0} { set timer [string range $time 1 end] } { set timer [expr $time * 60] }
-if {[lsearch -glob [utimers] "* go *"] == -1} { utimer $timer go }
-
-proc go {} {
-  global channel time text timer
-  foreach chan $channel {
-    set line [lindex $text [rand [llength $text]]]
-    putserv "PRIVMSG $chan :$line"
-  }
-  if {[lsearch -glob [utimers] "* go *"] == -1} { utimer $timer go }
-}
-
-bind dcc m timer chk_timer
-proc chk_timer {hand args} {
-  putlog [timers]
-}
-bind dcc m killtimer kill_timer
-proc kill_timer {idx args} {
-  set dead [lindex $args 1]
-  killtimer $dead
-  putlog "Killed timer $dead"
-}
-  
-set timers [timers]
-if { ![string match *hide_away* $timers] } {
-  timer 10 "hide_away"
-}
-
-putlog "mytukok voice.tcl loaded"    
-  
+putlog "vian story.tcl loaded"
